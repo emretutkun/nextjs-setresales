d575ba483c27a57920a99ca5fc4ed8884f72bcac
EMRE TUTKUN <denizci_emre98@hotmail.com>
2022-12-29 16:55:27 +0300

    bug fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,24 @@
 /* eslint-disable @next/next/no-img-element */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 import axios from "axios";
 import React from "react";
+import Head from "next/head";
+import Link from 'next/link'
+
+  
+
 const App = () => {
   const [data, setData] = useState<any>();
   const [isLoading, setIsLoading] = useState(false);
   const [err, setErr] = useState("");
   const [title, setTitle] = useState("");
+  const [ItemCode, setItemCode] = useState("");
+
   var name;
   const submitContact = async (event: any) => {
     event.preventDefault();
-
     setIsLoading(true);
-
     try {
       const response = await axios.get(
         `https://api.setre.com/api/SalesRemainingReport?STCode=${title}`
@@ -28,11 +33,21 @@ const App = () => {
       setIsLoading(false);
     }
   };
-
   console.log(data);
   var photo;
+
+
+
+
+  
   return (
+    
     <div>
+        <Head>
+        <title>Setre Satış Kalan</title>
+        <meta name="Setre Satış Kalan" property="og:title" content="My page title" key="title" />
+
+        </Head>
       {err && <h2>{err}</h2>}
       <div className={styles.main}>
         <img className={styles.logon} src="/logonw.jpg" alt="/logo.jpg" />
@@ -40,12 +55,13 @@ const App = () => {
           <input
             className={styles.input}
             id="name"
-            placeholder="bknz:el0521"
+            placeholder="bknz:el0521 "
             name="name"
             type="text"
             onChange={(event) => setTitle(event.target.value)}
             required
           />
+          
           <button
             className={styles.button}
             placeholder="bknz:el0521"
@@ -54,20 +70,30 @@ const App = () => {
             ARA
           </button>
         </div>
-        <br></br>
+        
 
-        {isLoading && (
-          <img src="/Loading.gif" className={styles.loadingtext} alt="" />
-        )}
-        {data &&
+        {isLoading && 
+          <img src="/Loading.gif" className={styles.limage} alt="" />
+        }<br></br>
+         {data &&
           data.map((person: any) => {
             return (
               person.colorDescription == data[0].colorDescription && (
-                <img src={person.photo} className={styles.image} alt="" />
+                <div>
+                <div className={styles.iki}><img src={person.photo} className={styles.image} alt="" />  </div>
+   
+                <div className={styles.dataloader}>
+                <div className={styles.üc}><a className={styles.bas}>MALİYET : </a>{person.maliyet}₺</div>
+                <div className={styles.bir}><a className={styles.bas}>P.SATIŞ : </a>{person.psf}₺</div>
+                </div>
+                </div>
               )
             );
-          })}
-        <br></br>
+          })} 
+                          
+
+          
+        
       </div>
 
       <div className={styles.debene}>
@@ -75,6 +101,7 @@ const App = () => {
         <div className={styles.gösterbas}>Üretim</div>
         <div className={styles.gösterbas}>Satış</div>
         <div className={styles.gösterbas}>Kalan</div>
+        <div className={styles.gösterbas}>Yüzde</div>
 
         {data &&
           data.map((person: any) => {
@@ -92,6 +119,9 @@ const App = () => {
                 <div className={styles.gösterana}>
                   <p className={styles.p}>{person.kalan}</p>
                 </div>
+                <div className={styles.gösterana}>
+                  <p className={styles.p}>{person.itemDescription}</p>
+                </div>
               </div>
             );
           })}
@@ -100,4 +130,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default App ;
